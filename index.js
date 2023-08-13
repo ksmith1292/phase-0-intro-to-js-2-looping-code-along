3f4251bfabc78131f0980fab5c58f8209f3acaba
Kevin <kevin_smith1292@yahoo.com>
2023-08-13 16:10:30 -0500

    yea boi

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+const newCards=[]
+  for (let i = 0; i < cards.length; i++) {
+    newCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return newCards;
+}
+writeCards(cards);
+
+
+function countDown() { 
+    let counDwn = 10;
+    while (counDwn > -1){
+        console.log(counDwn--)
+    }}
+   
+
+    
